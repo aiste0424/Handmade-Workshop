d37c85fa6dc7e451481244edb53e48c5e2d016cc
Aiste0424 <aiste0424@gmail.com>
2022-02-01 19:48:49 +0000

    pyramid beginning

---
+++ Handmade/Pyramid.cpp
@@ -0,0 +1,33 @@
+#include "Pyramid.h"
+
+Pyramid::Pyramid(GLfloat width, GLfloat height, GLfloat depth, GLfloat r, GLfloat g, GLfloat b, GLfloat a)
+{
+}
+
+Pyramid::~Pyramid()
+{
+}
+
+void Pyramid::SetTextureScale(GLfloat width, GLfloat height)
+{
+}
+
+void Pyramid::SetColor(const glm::vec4& color)
+{
+}
+
+void Pyramid::SetColor(GLfloat r, GLfloat g, GLfloat b, GLfloat a)
+{
+}
+
+void Pyramid::SetDimension(const glm::vec3& dimension)
+{
+}
+
+void Pyramid::SetDimension(GLfloat width, GLfloat height, GLfloat depth)
+{
+}
+
+void Pyramid::Render(Shader& shader)
+{
+}
